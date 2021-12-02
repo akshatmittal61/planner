7a381ed81b6877a14028dce31bebddf125c5ca05
akshatmittal61 <akshatmittal2506@gmail.com>
2021-12-02 14:09:24 +0530

    Merged frontend and backend

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
-import { fileURLToPath } from 'url'
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = dirname(__fileName);
@@ -12,9 +13,31 @@ app.use(bodyParser.json());
 let events = [];
 let notes = [];
 let tasks = [];
+fs.readFile(path.join(__dirname, "events.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    events = [...JSON.parse(data)];
+})
+fs.readFile(path.join(__dirname, "notes.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    notes = [...JSON.parse(data)];
+})
+fs.readFile(path.join(__dirname, "tasks.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    tasks = [...JSON.parse(data)];
+})
+const writeData = (arr, str) => {
+    fs.writeFile(path.join(__dirname, `${str}.json`), JSON.stringify(arr), (err) => {
+        if (err) throw err;
+        console.log(`File Written to ${__dirname}/${str}.json`);
+    })
+}
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    // res.sendFile(path.join(__dirname, 'index.html'));
+    res.json(events, notes, tasks);
 })
 app.get('/api/events', (req, res) => {
     res.json(events);
@@ -43,6 +66,7 @@ app.get('/events', (req, res) => {
 app.post('/events', (req, res) => {
     let newEvent = req.body;
     events = [...events, newEvent];
+    writeData(events, "events");
     res.json(newEvent);
 })
 app.patch('/events/:id', (req, res) => {
@@ -52,6 +76,7 @@ app.patch('/events/:id', (req, res) => {
     events[id].description = newEvent.description;
     events[id].type = newEvent.type;
     events[id].date = newEvent.date;
+    writeData(events, "events");
     res.json(newEvent);
 })
 app.delete('/events/:id', (req, res) => {
@@ -62,6 +87,7 @@ app.delete('/events/:id', (req, res) => {
         event.id = index;
     })
     events = [...newArray];
+    writeData(events, "events");
     res.json(events);
 })
 
@@ -71,6 +97,7 @@ app.get('/notes', (req, res) => {
 app.post('/notes', (req, res) => {
     let newNote = req.body;
     notes = [...notes, newNote];
+    writeData(notes, "notes");
     res.json(newNote);
 })
 app.patch('/notes/:id', (req, res) => {
@@ -81,6 +108,7 @@ app.patch('/notes/:id', (req, res) => {
     notes[id].linkURL = newNote.linkURL;
     notes[id].linkText = newNote.linkText;
     notes[id].color = newNote.color;
+    writeData(notes, "notes");
     res.json(newNote);
 })
 app.delete('/notes/:id', (req, res) => {
@@ -91,6 +119,7 @@ app.delete('/notes/:id', (req, res) => {
         note.id = index;
     })
     notes = [...newArray];
+    writeData(notes, "notes");
     res.json(notes);
 })
 
@@ -100,6 +129,7 @@ app.get('/tasks', (req, res) => {
 app.post('/tasks', (req, res) => {
     let newTask = req.body;
     tasks = [...tasks, newTask];
+    writeData(tasks, "tasks");
     res.json(newTask);
 })
 app.patch('/tasks/:id', (req, res) => {
@@ -110,6 +140,7 @@ app.patch('/tasks/:id', (req, res) => {
     tasks[id].date = newTask.date;
     tasks[id].time = newTask.time;
     tasks[id].done = newTask.done;
+    writeData(tasks, "tasks");
     res.json(newTask);
 })
 app.delete('/tasks/:id', (req, res) => {
@@ -120,6 +151,7 @@ app.delete('/tasks/:id', (req, res) => {
         task.id = index;
     })
     tasks = [...newArray];
+    writeData(tasks, "tasks");
     res.json(tasks);
 })
 
