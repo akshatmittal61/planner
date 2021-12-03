d8c2a00179e283af1822ad54eecece36597ad05e
akshatmittal61 <akshatmittal2506@gmail.com>
2021-12-04 02:41:10 +0530

    Fix baseURL: 0

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
 		setsideBarExpand((window.innerWidth > 880 ? true : false));
 	}
 	const axiosInstance = axios.create({
-		baseURL: 'http://localhost:5000'
+		baseURL: ''
 	})
 	return (
 		<Router>
