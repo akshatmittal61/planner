7a1f484bb14f0780c2ab7c288386a81b7d9e1e46
Akshat Mittal <akshatmittal2506@gmail.com>
2022-06-14 18:43:48 +0530

    added transition in home page icon

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,40 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import people from "../../images/people.svg";
 import favicon from "../../images/favicon.svg";
 
 const Home = () => {
+	const [imgScale, setImgScale] = useState(1);
+	const [showNavigation, setShowNavigation] = useState(false);
+	useEffect(() => {
+		if (imgScale > 100) {
+			setTimeout(() => {
+				setShowNavigation(true);
+			}, 3000);
+		}
+	}, [imgScale]);
+
 	return (
-		<main class="home">
-			<div class="card">
+		<main className="home">
+			<div className="card">
 				<div
-					class="card-frame"
+					className="card-frame"
 					data-aos="fade-in"
 					data-aos-duration={2000}
 				>
-					<div class="card-box">
-						<div class="home-box-head" data-aos="zoom-out">
+					<div className="card-box">
+						<div className="home-box-head" data-aos="zoom-out">
 							<div
-								class="home-box-image"
+								className="home-box-image"
 								style={{
 									backgroundImage: `url(${people})`,
 								}}
 							>
 								<img
-									class="home-box-image__img"
+									className="home-box-image__img"
 									src={favicon}
 									alt="favicon"
+									style={{
+										transform: `scale(${imgScale})`,
+										transition: "all 2.5s linear",
+									}}
+									onClick={() => {
+										setImgScale(500);
+									}}
 								/>
 							</div>
-							<div class="home-box-title">
-								<span class="home-box-title__text">
-									Planner
-								</span>
-							</div>
+							{imgScale < 10 && (
+								<div className="home-box-title">
+									<span className="home-box-title__text">
+										Planner
+									</span>
+								</div>
+							)}
 						</div>
 					</div>
 				</div>
 			</div>
+			{showNavigation && (
+				<section className="home-navigation">
+					Calendar Events Notes Tasks
+				</section>
+			)}
 		</main>
 	);
 };
