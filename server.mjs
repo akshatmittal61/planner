5456b7b226d588c8b135f3c73cef604597d1d9f9
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-28 10:20:28 +0530

    initial server setup

---
+++ server.mjs
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+
+config();
+const app = express();
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_CONNECTION_URL);
+		console.log("Connected to MongoDB successfuly.");
+	} catch (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+};
+
+app.use(cors("*"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	// connectDB();
+	console.log(`Server started at port ${PORT}`);
+});
