53dc8a9ccc31c8aea8203c4dcfaf30f56b2c9aca
Akshat Mittal <akshatmittal2506@gmail.com>
2022-07-31 09:16:57 +0530

    get single task route

--- controllers/tasks.mjs
+++ controllers/tasks.mjs
@@ -12,4 +12,21 @@ const getAllTaks = async (req, res) => {
 	}
 };
 
-export { getAllTaks };
+const getTask = async (req, res) => {
+	const id = req.params.id;
+	try {
+		const foundTask = await Task.findById(id);
+		if (!foundTask)
+			return res.status(404).json({ message: "Task not found" });
+		if (foundTask.user.toString() !== req.user.id)
+			return res.status(401).json({ message: "User not authorized" });
+		return res.status(200).json({ foundTask });
+	} catch (error) {
+		console.error(error);
+		if (error.kind === "ObjectId")
+			return res.status(404).json({ message: "Task Not found" });
+		return res.status(500).json({ message: "Server Error" });
+	}
+};
+
+export { getAllTaks, getTask };
--- routes/tasks.mjs
+++ routes/tasks.mjs
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllTaks } from "../controllers/tasks.mjs";
+import { getAllTaks, getTask } from "../controllers/tasks.mjs";
 import auth from "../middleware/auth.mjs";
 
 const router = Router();
 
 router.get("/", auth, getAllTaks);
+router.get("/:id", auth, getTask);
 
 export default router;
