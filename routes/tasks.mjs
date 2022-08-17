cca1dc7feb2e24dd041583b1e7940fc3752f5d40
Akshat Mittal <akshatmittal2506@gmail.com>
2022-08-17 23:56:51 +0530

    created mark as done route

--- routes/tasks.mjs
+++ routes/tasks.mjs
@@ -5,6 +5,7 @@ import {
 	editTask,
 	getAllTaks,
 	getTask,
+	markAsDone,
 } from "../controllers/tasks.mjs";
 import auth from "../middleware/auth.mjs";
 
@@ -15,6 +16,7 @@ router.get("/", getAllTaks);
 router.get("/:id", getTask);
 router.post("/add", addTask);
 router.put("/edit/:id", editTask);
+router.put("/mark-as-done/:id", markAsDone);
 router.delete("/delete/:id", deteleTask);
 
 export default router;
