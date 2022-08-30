a471bf41f8e40ce9e0a5487431ca7cc46dc437d3
Akshat Mittal <akshatmittal2506@gmail.com>
2022-08-30 08:53:07 +0530

    created register and unregister service worker file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import register from "./utils/service-worker";
 
 ReactDOM.render(
 	<BrowserRouter>
@@ -9,3 +10,5 @@ ReactDOM.render(
 	</BrowserRouter>,
 	document.getElementById("root")
 );
+
+register();
