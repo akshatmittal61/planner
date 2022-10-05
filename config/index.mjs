1011558ef2daccbdb01b676987585868153b4e6a
akshat-khosya <76739180+akshat-khosya@users.noreply.github.com>
2022-10-05 14:28:10 +0530

    config

--- config/index.mjs
+++ config/index.mjs
@@ -1,7 +1,35 @@
-import { config } from "dotenv";
+ import env from "dotenv";
 
-config();
+// config();
 
-export const PORT = process.env.PORT;
-export const dbUri = process.env.MONGO_CONNECTION_URL;
-export const jwtSecret = process.env.JWT_SECRET;
+// export const PORT = process.env.PORT;
+// export const dbUri = process.env.MONGO_CONNECTION_URL;
+// export const jwtSecret = process.env.JWT_SECRET;
+
+class Config{
+    _config={}
+    constructor(){
+        env.config();
+        this._config={
+            PORT=process.env.PORT,
+            dbUri=process.env.MONGO_CONNECTION_URL,
+            jwtSecret=process.env.JWT_SECRET
+        }
+    }
+    get(key) {
+        const val = this._config[key] ?? null;
+
+        if (!val) {
+            throw new Error(`Config for key [${key}] not found`);
+        }
+
+        return val;
+    }
+    set(key, val) {
+        this._config[key] = val;
+    }
+}
+
+const config = new Config();
+
+export default config;
